60220caf67b13d04105b9c0d483ef8e2b30038ac
Matthew244Z <m244zhu@uwaterloo.ca>
2021-06-15 20:56:52 -0500

    paintBoard function created

--- tictactoeJS.js
+++ tictactoeJS.js
@@ -1,6 +1,8 @@
 const BOARD_NUM = 9;
 
-console.log("This is the javascript tic tac toe")
+console.log("This is the javascript tic tac toe");
+
+const boardColor = [];
 
 const createBoard = function () {
 
@@ -9,8 +11,23 @@ const createBoard = function () {
     for(let i = 0; i < BOARD_NUM; i++)
     {
         document.querySelector(".boardList").innerHTML += '<li class = "boardSquare board' +i+'"></li>';
-
+        boardColor[i] = "white";
     }
 }
 
+const paintBoard = function (array) {
+
+    for(let i = 0; i < BOARD_NUM; i++)
+    {
+        document.querySelector(".board" + i).style.backgroundColor = array[i];
+    }
+
+}
+
 createBoard();
+boardColor[0] = "yellow";
+paintBoard(boardColor);
+
+// document.querySelector(".board").innerHTML = "<p> Hello World! </p>";
+// addEventListener
+// removeEventListener
