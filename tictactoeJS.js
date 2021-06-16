07d2fb4421642f447179c38b8e9a43e49abbc72a
Matthew244Z <m244zhu@uwaterloo.ca>
2021-06-15 22:42:37 -0500

    winner created

--- tictactoeJS.js
+++ tictactoeJS.js
@@ -18,14 +18,14 @@ const createBoard = function () {
     }
 }
 
-const playerOnePlaying = function () {
-    if (playerOneTurn)
-        updateColor = "yellow";
-    else 
-        updateColor = "blue";
+// const playerOnePlaying = function () {
+//     if (playerOneTurn)
+//         updateColor = "yellow";
+//     else 
+//         updateColor = "blue";
 
-    playerOneTurn = !playerOneTurn;
-}
+//     playerOneTurn = !playerOneTurn;
+// }
 
 
 // const paintBoard = function (array) {
@@ -40,7 +40,7 @@ const playerOnePlaying = function () {
 const turnOn = function () {
     if (turn > MAX_TURN)
         return false;
-    
+    return true;
 }
 
 const changeColor = function () {
@@ -51,15 +51,50 @@ const changeColor = function () {
     else
         turnColor = "blue"
 }
+
+const isWinner = function (array) {
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+
+    // check horizontals: increments of 1
+    // check verticals: increments of 3
+    // check diagonal 0 4 8
+    // check diagonal 2 4 6 
+
+    // check for horizontal and vertical winner
+    for (let i = 0; i < 3; i++)
+    {
+        // check for horizontal winner
+        // starts at 0 3 and 6, with increments of 1
+        let indexH = i*3;
+        if (array[indexH] == array[indexH+1] && array[indexH] == array[indexH+2] && array[indexH] != "white")
+            return true;
+        
+        // check for vertical winner
+        // starts at 0 1 and 2, with increments of 3
+        let indexV = i;
+        if (array[indexV] == array[indexV+3] && array[indexV] == array[indexV+6] && array[indexV] != "white")
+            return true;
+    }
+
+    // check for diagonal 0 4 8
+    if (array[0] == array[4] && array[0] == array[8] && array[0] != "white")
+        return true;
+
+    // check for diagonal 2 4 6
+    if (array[2] == array[4] && array[2] == array[6] && array[2] != "white")
+        return true;
+}
+
 const updateBoard = function (index) {
     let errorMessage = document.querySelector(".error");
-
+    let playerTurn = document.querySelector(".player");
+    
     // game is over, no more space 
-    if(turnOn())
+    if(!turnOn())
         return false;
 
-
-    // if winner
 
 
     // check if the square is valid 
@@ -77,6 +112,13 @@ const updateBoard = function (index) {
     // update color on array, so it won't be used again
     boardColor[index] = turnColor;
 
+    // game is over, there is a winner
+    if(isWinner(boardColor))
+    {
+        playerTurn.innerHTML = `Player ` + ( (turn+1)%2 + 1 ) + ` won!`;
+        turn = MAX_TURN;
+    }
+
     turn++;
     changeColor();
 
@@ -86,6 +128,7 @@ const updateBoard = function (index) {
 let status = document.querySelector(".infoGame");
 status.innerHTML =`
     <p>Info Game</p>
+    <p class="player"></p>
     <p class="error"></p>
 `;
 createBoard();
