d1cc70a740738886a1ae5ec8c971e7104ec343f1
Matthew244Z <m244zhu@uwaterloo.ca>
2021-06-15 22:01:39 -0500

    color added

--- tictactoeJS.js
+++ tictactoeJS.js
@@ -1,19 +1,32 @@
 const BOARD_NUM = 9;
-
-console.log("This is the javascript tic tac toe");
-
 const boardColor = [];
+const MAX_TURN = 9;
+let turn = 1;
+let playerOneTurn = true;
+let turnColor = "yellow";
+
 
 const createBoard = function () {
 
-    document.querySelector(".board").innerHTML = '<ul class="boardList"></ul>';
+    document.querySelector(".board").innerHTML = `<ul class="boardList"></ul>`;
 
     for(let i = 0; i < BOARD_NUM; i++)
     {
-        document.querySelector(".boardList").innerHTML += '<li class = "boardSquare board' +i+'"></li>';
+        document.querySelector(".boardList").innerHTML += `<li class = "boardSquare board` +i+`"></li>`;
         boardColor[i] = "white";
+        // document.querySelector(".board" + i).addEventListener("click", () => {console.log("Clicked")})
     }
 }
+
+const playerOnePlaying = function () {
+    if (playerOneTurn)
+        updateColor = "yellow";
+    else 
+        updateColor = "blue";
+
+    playerOneTurn = !playerOneTurn;
+}
+
 
 const paintBoard = function (array) {
 
@@ -24,9 +37,37 @@ const paintBoard = function (array) {
 
 }
 
+const turnOn = function () {
+    if (turn > MAX_TURN)
+        return false;
+    
+}
+
+const changeColor = function (index) {
+    if(boardColor[index] != "white")
+    {
+        document.querySelector(".error").innerHTML = `The selected square has already been filled. Select a different square.`
+        return false;
+    }
+
+    document.querySelector(".board" + index).style.backgroundColor = turnColor;
+    
+    return true;
+}
+
+let status = document.querySelector(".infoGame");
+status.innerHTML =`
+    <p>Info Game</p>
+    <p class="error"></p>
+`;
 createBoard();
-boardColor[0] = "yellow";
 paintBoard(boardColor);
+
+// I tried to run this inside createBoard, but only the last item was working
+for(let i = 0; i < BOARD_NUM; i++)
+{
+    document.querySelector(".board" + i).addEventListener("click", () => {changeColor(i)})
+}
 
 // document.querySelector(".board").innerHTML = "<p> Hello World! </p>";
 // addEventListener
